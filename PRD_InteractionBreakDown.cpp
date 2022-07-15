0af5c00a86e66318ef44f2fe2ab13140d6a1351c
afropapp13 <afropapp@yahoo.gr>
2022-07-15 17:07:58 -0500

    July 15 2022: 1) margin frame spaces

--- PRD_InteractionBreakDown.cpp
+++ PRD_InteractionBreakDown.cpp
@@ -181,7 +181,7 @@ void PRD_InteractionBreakDown(TString BaseMC = "") {
 				midPad->SetBottomMargin(0.03);
 				midPad->SetTopMargin(0.03);
 				botPad->SetTopMargin(0.03);
-				botPad->SetBottomMargin(0.26);
+				botPad->SetBottomMargin(0.27);
 				botPad->SetGridx();
 				botPad->SetGridy();
 				topPad->Draw();
